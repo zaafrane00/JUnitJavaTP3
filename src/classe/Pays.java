d3cccbfdf91561fe3868797891d4037c1624f69f
hamza zaafrane <zaafranehamza@gmail.com>
2020-12-18 09:39:20 +0100

    pays finished

--- src/classe/Pays.java
+++ src/classe/Pays.java
@@ -1,6 +1,7 @@
 package classe;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Pays {
@@ -8,7 +9,7 @@ public class Pays {
     private List<Person> Persons;
     private int personsLength;
 
-    public Pays(String label,) {
+    public Pays(String label) {
         this.label = label;
         Persons = new ArrayList<Person>();
     }
@@ -18,5 +19,30 @@ public class Pays {
         System.out.println(p.getNom()+"added");
     }
 
-    public deletePeron(int id){}
+    public void deletePeron(int id){
+        Person x=this.Persons.get(id);
+        if(x!=null){
+            this.Persons.remove(id);
+            System.out.println(x.getNom()+"deleted");
+        }else{
+            System.out.println("Not Found");
+        }
+    }
+
+    public void editPerson(Person p){
+       Boolean ok=this.Persons.contains(p);
+       if(ok){
+           int pos=this.Persons.indexOf(p);
+           //Person x=this.Persons.get(pos);
+           this.Persons.set(pos,p);
+           System.out.println(p.getNom()+"updated");
+       }else{
+           System.out.println(p.getNom()+"Not Found");
+       }
+    }
+
+    public void affiche(){
+        System.out.println(Arrays.deepToString(this.Persons.toArray()));
+    }
+
 }
